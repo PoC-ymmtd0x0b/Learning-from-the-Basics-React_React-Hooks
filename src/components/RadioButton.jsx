bf2193134e5a7a3ef1bec954b97c09440a5c6f4f
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-10-18 01:12:19 +0900

    ラジオボタンの生成にmapを利用する

--- src/components/RadioButton.jsx
+++ src/components/RadioButton.jsx
@@ -1,39 +1,34 @@
 import React, { useState } from "react";
 
+const values = [
+  { id: 1, item: "赤" },
+  { id: 2, item: "青" },
+  { id: 3, item: "黄" },
+];
+
+const RadioBtnItems = ({ onChange, checked }) =>
+  values.map((value) => {
+    return (
+      <label key={value.id}>
+        <input
+          type="radio"
+          value={value.item}
+          checked={checked === value.item}
+          onChange={() => onChange(value.item)}
+        />
+        {value.item}
+      </label>
+    );
+  });
+
 export default function RadioButton() {
-  const [checkedValue, setCheckedValue] = useState("赤");
+  const [checkedValue, setCheckedValue] = useState(values[0]["item"]);
   const handleChange = (value) => setCheckedValue(value);
 
   return (
     <>
       <p>現在選択されている値：{checkedValue}</p>
-      <label>
-        <input
-          type="radio"
-          value="赤"
-          checked={checkedValue === "赤"}
-          onChange={(e) => handleChange(e.target.value)}
-        />
-        赤
-      </label>
-      <label>
-        <input
-          type="radio"
-          value="青"
-          checked={checkedValue === "青"}
-          onChange={(e) => handleChange(e.target.value)}
-        />
-        青
-      </label>
-      <label>
-        <input
-          type="radio"
-          value="黄"
-          checked={checkedValue === "黄"}
-          onChange={(e) => handleChange(e.target.value)}
-        />
-        黄
-      </label>
+      <RadioBtnItems onChange={handleChange} checked={checkedValue} />
     </>
   );
 }
