1428d02a22d7d70255303c57f001987028eb6a34
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-10-19 00:23:44 +0900

    クリーンアップ関数の実行

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,50 @@ import React, { useState, useEffect } from "react";
 
 const INITIAL_COUNT = 0;
 
-const SampleComponent = () => {
+const Timer = () => {
   const [count, setCount] = useState(INITIAL_COUNT);
 
   const callbackFanction = () => {
-    document.title = `${count} 回クリックされました`;
+    alert("副作用関数が実行されました！");
+    const timer = setInterval(countIncrement, 1000);
+
+    return () => {
+      console.log("timerが削除されました！");
+      clearInterval(timer);
+    };
   };
 
-  useEffect(callbackFanction, [count]);
+  useEffect(callbackFanction, []);
 
-  const countIncrement = () => setCount(count + 1);
+  const countIncrement = () => {
+    setCount(count + 1);
+    console.log("カウントアップ +1");
+  };
   const countReset = () => setCount(INITIAL_COUNT);
 
   return (
     <div>
       <p>現在のカウント数：{count}</p>
-      <button onClick={countIncrement}>+ 1 ボタン</button>
       <button onClick={countReset}>リセット</button>
     </div>
   );
 };
 
 function App() {
-  return <SampleComponent />;
+  const [display, toggleDisplay] = useState(false);
+  const handleToggleDisplay = () => {
+    toggleDisplay(!display);
+  };
+
+  return (
+    <>
+      <button onClick={handleToggleDisplay}>
+        {display ? "タイマーを非表示" : "タイマーを表示"}
+      </button>
+
+      {display && <Timer />}
+    </>
+  );
 }
 
 export default App;
