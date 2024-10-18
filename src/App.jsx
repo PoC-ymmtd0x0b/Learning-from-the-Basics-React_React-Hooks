22a802734c7fead55bbb5bcd48809e259c1c1156
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-10-18 23:51:28 +0900

    単純なカウントボタンをトリガーにuseEffectを動作させる

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const INITIAL_COUNT = 0;
+
+const SampleComponent = () => {
+  const [count, setCount] = useState(INITIAL_COUNT);
+
+  const callbackFanction = () => {
+    document.title = `${count} 回クリックされました`;
+  };
+
+  useEffect(callbackFanction, [count]);
+
+  const countIncrement = () => setCount(count + 1);
+  const countReset = () => setCount(INITIAL_COUNT);
+
+  return (
+    <div>
+      <p>現在のカウント数：{count}</p>
+      <button onClick={countIncrement}>+ 1 ボタン</button>
+      <button onClick={countReset}>リセット</button>
+    </div>
+  );
+};
 
 function App() {
-  return <div>Hello React!!</div>;
+  return <SampleComponent />;
 }
 
 export default App;
