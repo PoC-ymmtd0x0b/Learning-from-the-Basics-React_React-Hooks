c3b2963b3f8056b814c4ba6ef6de8c4a21576cd8
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-10-20 01:54:02 +0900

    シンプルなuseContenxtのサンプル

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
+
+const SampleContextObj = createContext();
+
+const ConsumerComponent = () => {
+  const messageText = useContext(SampleContextObj);
+
+  console.log(messageText);
+
+  return <p>{messageText}</p>;
+};
+
+const message = "I love React!!";
 
 function App() {
-  return <div>Hello React!!</div>;
+  return (
+    <SampleContextObj.Provider value={message}>
+      <ConsumerComponent />
+    </SampleContextObj.Provider>
+  );
 }
 
 export default App;
