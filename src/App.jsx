5bc39b8e783db7ae39a5f3120f4e64d8e81fe579
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-10-19 18:33:44 +0900

    ボタンをクリックしたら入力フォームにフォームされるコンポーネントを作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
+
+const SampleComponent = () => {
+  const inputRefObj = useRef(null);
+
+  const handleClick = () => {
+    inputRefObj.current.focus();
+  };
+
+  return (
+    <div>
+      <input type="text" ref={inputRefObj} />
+      <button onClick={handleClick}>入力エリアをフォーカス</button>
+    </div>
+  );
+};
 
 function App() {
-  return <div>Hello React!!</div>;
+  return <SampleComponent />;
 }
 
 export default App;
